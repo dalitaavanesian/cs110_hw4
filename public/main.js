a513536fd20ec249eb8e9625678694c1d2c4e7fc
Dalita <dalita.avanessian@gmail.com>
2016-10-31 23:06:26 +0400

    getting todos from server

--- public/main.js
+++ public/main.js
@@ -1,8 +1,18 @@
 $(function($) {
-  $('.check-button').click(function() {
-    $(this).toggleClass('hidden');
-    $(this).siblings('.check-button').toggleClass('hidden');
+
+
+  $.get('/gettodos', function(result) {
+    for(var i = 0; i < result.length; i++) {
+      var todo = result[i];
+      $('#todo-list ul').append('<li><i class="check-button fa fa-check-circle-o ' + (todo.checked ? '' : 'hidden') + '"></i><i class="check-button fa fa-circle-o ' + (todo.checked ? 'hidden' : '') + '"></i><i class="fa fa-trash-o"></i><span>' + todo.title + '</span></li>');
+    }
+    addCheckClickAction();
   });
 
-  
+  function addCheckClickAction() {
+    $('.check-button').click(function() {
+      $(this).toggleClass('hidden');
+      $(this).siblings('.check-button').toggleClass('hidden');
+    });
+  }
 });
